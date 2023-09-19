77c7ac450e0535ee38efd6b4592a3e5fb0191816
emily <emil7809@gmail.com>
2023-09-19 21:13:08 +0200

    added so sub au

--- src/components/filter/burger_menu_filter.tsx
+++ src/components/filter/burger_menu_filter.tsx
@@ -8,8 +8,14 @@ import {
     MenuOptionGroup,
     MenuDivider,
     IconButton,
+    Box,
+    Button,
   } from '@chakra-ui/react'
-  import { HamburgerIcon, AddIcon, ExternalLinkIcon, RepeatIcon, EditIcon} from '@chakra-ui/icons'
+  import { HamburgerIcon, CheckIcon, SmallAddIcon} from '@chakra-ui/icons'
+  import {FcGenericSortingAsc} from 'react-icons/fc';
+  import {LuListTodo} from 'react-icons/lu';
+  import {GiFeather} from 'react-icons/gi';
+
  
 
 
@@ -22,18 +28,52 @@ function BurgerMenuFilter(){
         variant={'outline'}
         />
         <MenuList>
-            <MenuItem icon={<AddIcon />} command='⌘T'>
-            New Tab
-            </MenuItem>
-            <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
-            New Window
-            </MenuItem>
-            <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
-            Open Closed Tab
-            </MenuItem>
-            <MenuItem icon={<EditIcon />} command='⌘O'>
-            Open File...
-            </MenuItem>
+           <Box padding={2} w='100vw'>
+                <h2>Filter</h2>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+                <Box textAlign={'left'} paddingLeft={8} marginTop={3} >
+                    <Box display={'flex'}>
+                        <FcGenericSortingAsc/>
+                        <h3>Sorting</h3>
+                    </Box>
+                    <Box>
+                        <Button colorScheme='teal'>Alphabetical  A-Z</Button>
+                        <Button>Highest ratings</Button>
+                    </Box>
+                </Box>
+                <Box textAlign={'left'} paddingLeft={8} marginTop={3} >
+                    <Box display={'flex'}>
+                        <LuListTodo/>
+                        <h3>Subdjekt</h3>
+                    </Box>
+                    <Box>
+                        <Button colorScheme='teal'>Subject<CheckIcon/></Button>
+                        <Button>Subject <SmallAddIcon/></Button>
+                        <Button>Subject <SmallAddIcon/></Button>
+                        <Button>Subject <SmallAddIcon/></Button>
+                        <Button>Subject <SmallAddIcon/></Button>
+                        <Button>Subject <SmallAddIcon/></Button>
+                    </Box>
+                </Box>
+                <Box textAlign={'left'} paddingLeft={8} marginTop={3}>
+
+                </Box>
+                <Box textAlign={'left'} paddingLeft={8} marginTop={3} >
+                    <Box display={'flex'}>
+                        <GiFeather/>
+                        <h3>Author</h3>
+                    </Box>
+                    <Box>
+                        <Button colorScheme='teal'>Author<CheckIcon/></Button>
+                        <Button>Author <SmallAddIcon/></Button>
+                        <Button>Author <SmallAddIcon/></Button>
+                        <Button>Author <SmallAddIcon/></Button>
+                        <Button>Author <SmallAddIcon/></Button>
+                        <Button>Author <SmallAddIcon/></Button>
+                    </Box>
+                </Box>
+           </Box>
+           
         </MenuList>    
         
     </Menu>
@@ -42,6 +82,4 @@ function BurgerMenuFilter(){
 
 export default BurgerMenuFilter;
 
-function translate3d(arg0: number, px: any, arg2: number, px1: any, arg4: number, px2: any): any {
-    throw new Error('Function not implemented.')
-}
+
