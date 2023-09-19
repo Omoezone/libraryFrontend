3e46f36c82d8dc98b9e59438e4ad6e79020c22f1
emily <emil7809@gmail.com>
2023-09-19 21:36:46 +0200

    added a slider and cleaned up code

--- src/components/filter/burger_menu_filter.tsx
+++ src/components/filter/burger_menu_filter.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 import {
     Menu,
     MenuButton,
@@ -10,16 +12,29 @@ import {
     IconButton,
     Box,
     Button,
+    Slider,
+    SliderTrack,
+    SliderFilledTrack,
+    SliderThumb,
+    SliderMark,
   } from '@chakra-ui/react'
   import { HamburgerIcon, CheckIcon, SmallAddIcon} from '@chakra-ui/icons'
   import {FcGenericSortingAsc} from 'react-icons/fc';
   import {LuListTodo} from 'react-icons/lu';
   import {GiFeather} from 'react-icons/gi';
+  import {TbRulerMeasure} from 'react-icons/tb';
 
  
 
 
 function BurgerMenuFilter(){
+    const [sliderValue, setSliderValue] = useState(50)
+
+    const labelStyles = {
+        mt: '2',
+        ml: '-2.5',
+        fontSize: 'sm',
+    }
     return <Menu> 
         <MenuButton display={'flex'}
         as={IconButton}
@@ -31,47 +46,99 @@ function BurgerMenuFilter(){
            <Box padding={2} w='100vw'>
                 <h2>Filter</h2>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
-                <Box textAlign={'left'} paddingLeft={8} marginTop={3} >
+            
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuGroup>
                     <Box display={'flex'}>
                         <FcGenericSortingAsc/>
                         <h3>Sorting</h3>
                     </Box>
-                    <Box>
+                    <MenuDivider/>
+                    <MenuOptionGroup>
                         <Button colorScheme='teal'>Alphabetical  A-Z</Button>
                         <Button>Highest ratings</Button>
-                    </Box>
-                </Box>
-                <Box textAlign={'left'} paddingLeft={8} marginTop={3} >
+                    </MenuOptionGroup>
+                </MenuGroup>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuGroup  >
                     <Box display={'flex'}>
                         <LuListTodo/>
                         <h3>Subdjekt</h3>
                     </Box>
-                    <Box>
+                    <MenuDivider/>
+                    <MenuOptionGroup>
                         <Button colorScheme='teal'>Subject<CheckIcon/></Button>
                         <Button>Subject <SmallAddIcon/></Button>
                         <Button>Subject <SmallAddIcon/></Button>
                         <Button>Subject <SmallAddIcon/></Button>
                         <Button>Subject <SmallAddIcon/></Button>
                         <Button>Subject <SmallAddIcon/></Button>
+                    </MenuOptionGroup>
+                </MenuGroup>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuGroup >
+                <Box display={'flex'}>
+                        <TbRulerMeasure/>
+                        <h3>Number of pages</h3>
                     </Box>
-                </Box>
-                <Box textAlign={'left'} paddingLeft={8} marginTop={3}>
-
-                </Box>
-                <Box textAlign={'left'} paddingLeft={8} marginTop={3} >
+                    <MenuDivider/>
+                    <MenuDivider/>
+                    <MenuDivider/>
+                    <MenuDivider/>
+                <Slider aria-label='slider-ex-6' onChange={(val) => setSliderValue(val)}>
+                    <SliderMark value={25} {...labelStyles}>
+                    25
+                    </SliderMark>
+                    <SliderMark value={50} {...labelStyles}>
+                    50
+                    </SliderMark>
+                    <SliderMark value={75} {...labelStyles}>
+                    75
+                    </SliderMark>
+                    <SliderMark
+                    value={sliderValue}
+                    textAlign='center'
+                    bg='teal'
+                    color='white'
+                    mt='-10'
+                    ml='-5'
+                    w='12'
+                    >
+                    {sliderValue}
+                    </SliderMark>
+                    <SliderTrack>
+                    <SliderFilledTrack />
+                    </SliderTrack>
+                    <SliderThumb />
+                </Slider>
+                </MenuGroup>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuDivider/>
+                <MenuGroup  >
                     <Box display={'flex'}>
                         <GiFeather/>
                         <h3>Author</h3>
                     </Box>
-                    <Box>
+                    <MenuDivider/>
+                    <MenuOptionGroup>
                         <Button colorScheme='teal'>Author<CheckIcon/></Button>
                         <Button>Author <SmallAddIcon/></Button>
                         <Button>Author <SmallAddIcon/></Button>
                         <Button>Author <SmallAddIcon/></Button>
                         <Button>Author <SmallAddIcon/></Button>
                         <Button>Author <SmallAddIcon/></Button>
-                    </Box>
-                </Box>
+                    </MenuOptionGroup>
+                </MenuGroup>
            </Box>
            
         </MenuList>    
