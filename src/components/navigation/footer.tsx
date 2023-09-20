e7489eea4c35b2d4a21fc4050037ed05f1d758d8
William Omø <williamjean.k.o@gmail.com>
2023-09-20 10:40:27 +0200

    Updated footer to have image and stuff

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,7 +1,9 @@
-import { Flex, Icon } from "@chakra-ui/react"
+import { Flex, Image, Icon, Box, VStack, Text } from "@chakra-ui/react"
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa"
+import logoName from "../../assets/logoName.png";
 
 const footer = () => {
+
     return (
         <Flex
         align="center"
@@ -12,12 +14,25 @@ const footer = () => {
         bottom="0" 
         left="0" 
         right="0" 
-        
-    >
-        <Icon as={FaFacebook} fontSize="24px" mr="2" color="blue.500" />
-        <Icon as={FaTwitter} fontSize="24px" mr="2" color="blue.400" />
-        <Icon as={FaInstagram} fontSize="24px" mr="2" color="pink.500" />
-        <Icon as={FaLinkedin} fontSize="24px" color="blue.800" />
+    >   
+        <VStack
+        spacing={10}
+        >
+            <Box h="1rem" marginBottom="2rem">
+                <Image src={logoName} w="6rem" h="6rem"/>
+            </Box>
+            <Box h="20px" px="2rem" py="1rem">
+                <Icon as={FaFacebook} fontSize="24px" mr="2" color="blue.500" />
+                <Icon as={FaTwitter} fontSize="24px" mr="2" color="blue.400" />
+                <Icon as={FaInstagram} fontSize="24px" mr="2" color="pink.500" />
+                <Icon as={FaLinkedin} fontSize="24px" color="blue.800" />
+            </Box>
+            <Box>
+                <Text fontSize="sm" color="white" textAlign="center">
+                    © 2021 Kea Library Management System. All rights reserved.
+                </Text>
+            </Box>
+        </VStack>
     </Flex>
     )
 }
