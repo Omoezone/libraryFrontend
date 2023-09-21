cbd6c05316d3c073464e8fafe7f0f0e8c07ac21f
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-09-21 12:17:41 +0200

    add home component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Show } from "@chakra-ui/media-query";
 import "./App.css";
 import NavBar from "./components/navigation/navbar";
 import FooterBar from "./components/navigation/footer"
-
+import {getFavoriteBooks, getSavedBooks, getSubjectsBooks} from "./components/home/Home"
 
 function App() {
+
+
   return (
     <>
       <Grid
@@ -18,9 +20,39 @@ function App() {
         <GridItem gridArea="nav" >
           <NavBar />
         </GridItem>
+
         <GridItem gridArea="main">
-          // main grid 
+          
+          <div>
+            <h2>favorite books</h2>
+              <ul>
+                {getFavoriteBooks()}  
+              </ul>
+          </div>
+          
+          <br/>
+          <br/>
+          
+          <div>
+            
+            <h2>Saved books</h2>
+              <ul>
+                {getSavedBooks()}  
+              </ul>
+          </div>
+
+          <br/>
+          <br/>
+
+          <div>
+            <h2>Subjects</h2>
+              <ul>
+                {getSubjectsBooks()}  
+              </ul>
+          </div>
+
         </GridItem>
+        
         <GridItem gridArea="footer" >
           <FooterBar />
         </GridItem>
