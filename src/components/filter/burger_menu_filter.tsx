ad89ac4fabd59e1c212cb40a07a2fb15f1d16190
emily <emil7809@gmail.com>
2023-09-21 13:01:26 +0200

    burger contet put in drawer

--- src/components/filter/burger_menu_filter.tsx
+++ src/components/filter/burger_menu_filter.tsx
@@ -27,97 +27,86 @@ function BurgerConetnt() {
         fontSize: 'sm',
     }
     return (
-        <>
+
+        <Box>
             <Box>
+                <Box display={'flex'}>
+                    <FcGenericSortingAsc />
+                    <h3>Sorting</h3>
+                </Box>
 
                 <Box>
-                    <Box display={'flex'}>
-                        <FcGenericSortingAsc />
-                        <h3>Sorting</h3>
-                    </Box>
-
-                    <Box>
-                        <Button colorScheme='teal'>Alphabetical  A-Z</Button>
-                        <Button>Highest ratings</Button>
-                    </Box>
-                </Box>
-
-
-
-
-                <Box  >
-                    <Box display={'flex'}>
-                        <LuListTodo />
-                        <h3>Subdjekt</h3>
-                    </Box>
-
-                    <Box>
-                        <Button colorScheme='teal'>Subject<CheckIcon /></Button>
-                        <Button>Subject <SmallAddIcon /></Button>
-                        <Button>Subject <SmallAddIcon /></Button>
-                        <Button>Subject <SmallAddIcon /></Button>
-                        <Button>Subject <SmallAddIcon /></Button>
-                        <Button>Subject <SmallAddIcon /></Button>
-                    </Box>
-                </Box>
-
-
-
-                <Box >
-                    <Box display={'flex'}>
-                        <TbRulerMeasure />
-                        <h3>Number of pages</h3>
-                    </Box>
-
-
-
-
-                    <Slider aria-label='slider-ex-6' onChange={(val) => setSliderValue(val)}>
-                        <SliderMark value={25} {...labelStyles}>
-                            25
-                        </SliderMark>
-                        <SliderMark value={50} {...labelStyles}>
-                            50
-                        </SliderMark>
-                        <SliderMark value={75} {...labelStyles}>
-                            75
-                        </SliderMark>
-                        <SliderMark
-                            value={sliderValue}
-                            textAlign='center'
-                            bg='teal'
-                            color='white'
-                            mt='-10'
-                            ml='-5'
-                            w='12'
-                        >
-                            {sliderValue}
-                        </SliderMark>
-                        <SliderTrack>
-                            <SliderFilledTrack />
-                        </SliderTrack>
-                        <SliderThumb />
-                    </Slider>
-                </Box>
-
-                <Box  >
-                    <Box display={'flex'}>
-                        <GiFeather />
-                        <h3>Author</h3>
-                    </Box>
-
-                    <Box>
-                        <Button colorScheme='teal'>Author<CheckIcon /></Button>
-                        <Button>Author <SmallAddIcon /></Button>
-                        <Button>Author <SmallAddIcon /></Button>
-                        <Button>Author <SmallAddIcon /></Button>
-                        <Button>Author <SmallAddIcon /></Button>
-                        <Button>Author <SmallAddIcon /></Button>
-                    </Box>
+                    <Button colorScheme='teal'>Alphabetical  A-Z</Button>
+                    <Button>Highest ratings</Button>
                 </Box>
             </Box>
 
-        </>)
+            <Box>
+                <Box display={'flex'}>
+                    <LuListTodo />
+                    <h3>Subdjekt</h3>
+                </Box>
+
+                <Box>
+                    <Button colorScheme='teal'>Subject<CheckIcon /></Button>
+                    <Button>Subject <SmallAddIcon /></Button>
+                    <Button>Subject <SmallAddIcon /></Button>
+                    <Button>Subject <SmallAddIcon /></Button>
+                    <Button>Subject <SmallAddIcon /></Button>
+                    <Button>Subject <SmallAddIcon /></Button>
+                </Box>
+            </Box>
+
+            <Box>
+                <Box display={'flex'}>
+                    <TbRulerMeasure />
+                    <h3>Number of pages</h3>
+                </Box>
+                <Slider aria-label='slider-ex-6' onChange={(val) => setSliderValue(val)}>
+                    <SliderMark value={25} {...labelStyles}>
+                        25
+                    </SliderMark>
+                    <SliderMark value={50} {...labelStyles}>
+                        50
+                    </SliderMark>
+                    <SliderMark value={75} {...labelStyles}>
+                        75
+                    </SliderMark>
+                    <SliderMark
+                        value={sliderValue}
+                        textAlign='center'
+                        bg='teal'
+                        color='white'
+                        mt='-10'
+                        ml='-5'
+                        w='12'
+                    >
+                        {sliderValue}
+                    </SliderMark>
+                    <SliderTrack>
+                        <SliderFilledTrack />
+                    </SliderTrack>
+                    <SliderThumb />
+                </Slider>
+            </Box>
+
+            <Box>
+                <Box display={'flex'}>
+                    <GiFeather />
+                    <h3>Author</h3>
+                </Box>
+
+                <Box>
+                    <Button colorScheme='teal'>Author<CheckIcon /></Button>
+                    <Button>Author <SmallAddIcon /></Button>
+                    <Button>Author <SmallAddIcon /></Button>
+                    <Button>Author <SmallAddIcon /></Button>
+                    <Button>Author <SmallAddIcon /></Button>
+                    <Button>Author <SmallAddIcon /></Button>
+                </Box>
+            </Box>
+        </Box>
+    )
 
 }
 
