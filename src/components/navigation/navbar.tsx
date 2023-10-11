d54e66fce389017f3b7725919976d76c7770ba43
William Omø <williamjean.k.o@gmail.com>
2023-10-11 16:23:43 +0200

    tried some hooks on nav bar

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -2,12 +2,32 @@ import { HStack, Image, Spacer, Text} from "@chakra-ui/react";
 import BurgerMenu from "./burger";
 import SearchBar from "./searchbar";
 import Userpage from "../user/userpage";
+import useData from "../../hooks/useData";
+import { useState } from "react";
+
+interface Author {
+  id: number;
+  username: string;
+  total_books: string[];
+}
 
 const NavBar = () => {
+  const [id, setId] = useState(1); 
+  const { data, error } = useData<Author>("author/" + id);
+
   return (
   <>
     <HStack justifyContent="space-between" px={1} py={1}>
       <BurgerMenu />
+      <button onClick={() => setId(id + 1)}>Click me</button>
+      <div>
+        {data && (
+          <Text fontSize="lg" fontWeight="bold">
+            <p>{(data as Author).username}</p>
+            <p>{(data as Author).total_books}</p>
+          </Text>
+        )}
+      </div>
       {/* make this shit match with a grid maybe?*/}
       <Image src="assets/LogoName.svg" w="10%" h="10%" px={1} py={1} />
       <Spacer />
