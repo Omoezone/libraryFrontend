768ad7ca95ac0f87f9aa0d8959dda5416acadca1
William Omø <williamjean.k.o@gmail.com>
2023-12-05 17:15:42 +0100

    created signup test

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -3,16 +3,24 @@ describe('template spec', () => {
   beforeEach(() => {
     cy.clearAllCookies()
     cy.visit('http://localhost:5173/')
-  })
+  });
+  
   it('a new user is created successfully', () => {
-
-    cy.get('#hero_container').click();
     cy.get('.chakra-stack > .chakra-button:nth-child(5)').click();
-    cy.get('.css-1kfnfg8').click();
-    cy.get('.css-onkibi > path').click();
-    cy.get('.css-1hx9me4:nth-child(4)').click();
-    cy.get('.css-1kfnfg8').click();
+
+    cy.get('[name="first_name"]').type("cypress_first")
+    cy.get('[name="last_name"]').type("cypress_last")
+    cy.get('[name="email"]').type("cypress@mail.dk")
+    cy.get('[name="password"]').type("test")
     
+    cy.get('[name="first_name"]').should('have.value', "cypress_first");
+    cy.get('[name="last_name"]').should('have.value', "cypress_last");
+    cy.get('[name="email"]').should('have.value', "cypress@mail.dk");
+    cy.get('[name="password"]').should('have.value', "test");
+
+    cy.wait(2500)
+    // TODO: Dette skal med når vi er klar
+    //cy.get('[type="submit"]').click();
   })
 })
 
