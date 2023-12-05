bcf479d20f86ca76589a198c5ad8b45d5fa24b8c
William Omø <williamjean.k.o@gmail.com>
2023-12-05 16:42:58 +0100

    signup test begun

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,7 +1,18 @@
 
 describe('template spec', () => {
-  it('passes', () => {
+  beforeEach(() => {
+    cy.clearAllCookies()
     cy.visit('http://localhost:5173/')
+  })
+  it('a new user is created successfully', () => {
+
+    cy.get('#hero_container').click();
+    cy.get('.chakra-stack > .chakra-button:nth-child(5)').click();
+    cy.get('.css-1kfnfg8').click();
+    cy.get('.css-onkibi > path').click();
+    cy.get('.css-1hx9me4:nth-child(4)').click();
+    cy.get('.css-1kfnfg8').click();
+    
   })
 })
 
