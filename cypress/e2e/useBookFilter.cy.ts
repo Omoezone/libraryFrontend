aeb6f58c56ebfccc6feef71e06d8dd78d5977564
William Omø <williamjean.k.o@gmail.com>
2023-12-06 12:35:16 +0100

    cypress test

--- cypress/e2e/useBookFilter.cy.ts
+++ cypress/e2e/useBookFilter.cy.ts
@@ -5,11 +5,12 @@ describe('use the book filter', () => {
     })
 
     it('the filter is used to find a specific book correctly', () => {
-
+        
     })
 
     it('the tags are used to find a specific book correctly', () => {
-
+        cy.get('.chakra-button.css-1jw7goo:contains("Fantasy")').click();
+        cy.wait(5000)   
     })
 })
 
